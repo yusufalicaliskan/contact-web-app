38e434fef81b88fd64b07f93ceb6aae07eec5584
yusufalicaliskan <yusufalicaliskan@gmail.com>
2022-08-20 00:02:18 +0300

    add contact list

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -1,14 +1,26 @@
 import Style from "./style.module.css"
+import { useSelector, useDispatch } from 'react-redux'
 
 const Contacts = () => {
-    return (
-        <nav className={Style.contact}>
-            <ul>
-                <li>
+    const contacts = useSelector(state => state.contacts.value)
 
+    return (
+        <ul className={Style.list}>
+            {contacts.map((contact, index) => (
+                <li key={index} className={Style.item}>
+                    <button className={Style.button}>
+                        <div className={Style.image}>
+                            <img src={`https://joeschmoe.io/api/v1/${contact.name}`} className={Style.avatar} />
+                            <span className={`${Style.status} ${contact.status ? Style.statusOn : Style.statusOff}`}></span>
+                        </div>
+                        <div className={Style.content}>
+                            <h3 className={Style.name}>{contact.name} {contact.surname}</h3>
+                            <small className={Style.phone}>{contact.phone}</small>
+                        </div>
+                    </button>
                 </li>
-            </ul>
-        </nav>
+            ))}
+        </ul>
     )
 }
 
