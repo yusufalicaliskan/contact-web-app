cdc247cea7c49feafb871d475708a67de0ecef31
yusufalicaliskan <yusufalicaliskan@gmail.com>
2022-08-19 21:27:42 +0300

    add list

---
+++ src/components/Contacts/index.jsx
@@ -0,0 +1,15 @@
+import Style from "./style.module.css"
+
+const Contacts = () => {
+    return (
+        <nav className={Style.contact}>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Contacts
