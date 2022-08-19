a138f689feaea3afe705681b9330458d7f3b281e
yusufalicaliskan <yusufalicaliskan@gmail.com>
2022-08-20 00:00:32 +0300

    add router and toastcontainer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import App from './App';
 import store from './state'
 import { Provider } from 'react-redux'
@@ -8,7 +11,10 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <App />
+            <ToastContainer />
+        </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
