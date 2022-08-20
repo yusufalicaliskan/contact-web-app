104e247e9032df5421ccb19e7bc1a9864097d693
Yusuf Çalışkan <yusufalicaliskan@gmail.com>
2022-08-20 16:17:11 +0300

    fix blocked user color

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -21,7 +21,7 @@ const Contacts = () => {
                     <div className={Style.button}>
                         <div className={Style.image}>
                             <img src={`https://joeschmoe.io/api/v1/${contact.name}`} className={Style.avatar} />
-                            <span className={`${Style.status} ${contact.status ? Style.statusOn : Style.statusOff}`}></span>
+                            <span className={`${Style.status} ${!contact.status ? Style.statusOn : Style.statusOff}`}></span>
                         </div>
                         <div className={Style.content}>
                             <h3 className={Style.name}>{contact.name} {contact.surname}</h3>
