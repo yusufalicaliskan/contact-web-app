d21794cd45f159c5be811b0b6a17d8795430f4dd
Yusuf Çalışkan <yusufalicaliskan@gmail.com>
2022-08-20 16:18:11 +0300

    add apple loader

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,17 +3,38 @@ import Style from "./style.module.css"
 import Contacts from "../../components/Contacts";
 import Search from "../../components/Search";
 import CreateContact from "../../components/CreateContact";
+import { AiOutlinePlus, AiFillApple } from "react-icons/ai";
 
 const Home = () => {
     const [modal, setModal] = React.useState(false);
+    const [loader, setLoader] = React.useState(true);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setLoader(false)
+        }, 5000);
+    }, []);
 
     return (
         <div className={Style.wrapper}>
             <div className={Style.content}>
-                <button className={Style.createButton} onClick={() => setModal(true)}>Create Contact</button>
-                <Search />
-                <Contacts />
-                <CreateContact isOpen={modal} onClose={() => setModal(false)} />
+                {loader ? (
+                    <div className="progress-container">
+                        <span className="apple">
+                            <AiFillApple fill="#ffffff" size={45} />
+                        </span>
+                        <div className="progress" />
+                    </div>
+                ) : (
+                    <>
+                        <button className={Style.createButton} onClick={() => setModal(true)}>
+                            <AiOutlinePlus />
+                        </button>
+                        <Search />
+                        <Contacts />
+                        <CreateContact isOpen={modal} onClose={() => setModal(false)} />
+                    </>
+                )}
             </div>
         </div>
     )
