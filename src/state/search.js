7eb8cacf8905bb46d349bc28f989c60d47c42ab5
Yusuf Çalışkan <yusufalicaliskan@gmail.com>
2022-08-20 16:19:23 +0300

    fix search bug

--- src/state/search.js
+++ src/state/search.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit'
-import { toast } from "react-toastify"
 
 const initialState = {
     value: {
@@ -16,7 +15,7 @@ export const Search = createSlice({
             const { keyword, contacts } = action.payload.value;
 
             if(keyword.trim().length === 0) {
-                toast.error("Please enter the value");
+                state.value.search = []
             } else {
                 const searchKey = keyword.trim().toLocaleLowerCase()
                 state.value.search = contacts.filter(item => item.name.toLocaleLowerCase().includes(searchKey) || item.surname.toLocaleLowerCase().includes(searchKey))
