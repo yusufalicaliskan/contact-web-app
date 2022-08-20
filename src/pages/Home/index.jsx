b6add2281e1300af67fe7d3122ac3f5feddd7bef
Yusuf Çalışkan <yusufalicaliskan@gmail.com>
2022-08-20 17:34:33 +0300

    remove preload in home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,40 +3,20 @@ import Style from "./style.module.css"
 import Contacts from "../../components/Contacts";
 import Search from "../../components/Search";
 import CreateContact from "../../components/CreateContact";
-import { AiOutlinePlus, AiFillApple } from "react-icons/ai";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const Home = () => {
     const [modal, setModal] = React.useState(false);
-    const [loader, setLoader] = React.useState(true);
-
-    React.useEffect(() => {
-        setTimeout(() => {
-            setLoader(false)
-        }, 5000);
-    }, []);
 
     return (
-        <div className={Style.wrapper}>
-            <div className={Style.content}>
-                {loader ? (
-                    <div className="progress-container">
-                        <span className="apple">
-                            <AiFillApple fill="#ffffff" size={45} />
-                        </span>
-                        <div className="progress" />
-                    </div>
-                ) : (
-                    <>
-                        <button className={Style.createButton} onClick={() => setModal(true)}>
-                            <AiOutlinePlus />
-                        </button>
-                        <Search />
-                        <Contacts />
-                        <CreateContact isOpen={modal} onClose={() => setModal(false)} />
-                    </>
-                )}
-            </div>
-        </div>
+        <>
+            <button className={Style.createButton} onClick={() => setModal(true)}>
+                <AiOutlinePlus />
+            </button>
+            <Search />
+            <Contacts />
+            <CreateContact isOpen={modal} onClose={() => setModal(false)} />
+        </>
     )
 }
 
