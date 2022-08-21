4e36ac12b4df4f65ddb202601167e6f93b7b8a4f
yusufalicaliskan <yusufalicaliskan@gmail.com>
2022-08-21 09:30:16 +0300

    fix delete problem

--- src/components/Contacts/index.jsx
+++ src/components/Contacts/index.jsx
@@ -19,21 +19,24 @@ const Contacts = () => {
         <ul className={Style.list}>
             {(search.length > 0 ? search : contacts).map((contact, index) => (
                 <li key={index} className={Style.item}>
-                    <Link to={`/profile?name=${contact.name}&surname=${contact.surname}`} className={Style.button}>
-                        <div className={Style.image}>
-                            <img src={`https://joeschmoe.io/api/v1/${contact.name}`} className={Style.avatar} alt={contact.name} />
-                            <span className={`${Style.status} ${!contact.status ? Style.statusOn : Style.statusOff}`}></span>
-                        </div>
-                        <div className={Style.content}>
-                            <h3 className={Style.name}>{contact.name} {contact.surname}</h3>
-                            <small className={Style.phone}>{contact.phone}</small>
-                        </div>
-                        <div>
-                            <button className={Style.delete} onClick={() => deleteItem(contact)}>
-                                <AiOutlineDelete size={25} />
-                            </button>
-                        </div>
-                    </Link>
+                    <div>
+                        <Link to={`/profile?name=${contact.name}&surname=${contact.surname}`} className={Style.button}>
+                            <div className={Style.image}>
+                                <img src={`https://joeschmoe.io/api/v1/${contact.name}`} className={Style.avatar} alt={contact.name} />
+                                <span className={`${Style.status} ${!contact.status ? Style.statusOn : Style.statusOff}`}></span>
+                            </div>
+                            <div className={Style.content}>
+                                <h3 className={Style.name}>{contact.name} {contact.surname}</h3>
+                                <small className={Style.phone}>{contact.phone}</small>
+                            </div>
+                        </Link>
+                    </div>
+
+                    <div>
+                        <button className={Style.delete} onClick={() => deleteItem(contact)}>
+                            <AiOutlineDelete size={25} />
+                        </button>
+                    </div>
                 </li>
             ))}
         </ul>
